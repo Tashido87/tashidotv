90916d2362143807ef3c26a6b6ec533812c389dd
Tashido <herozboy@gmail.com>
2026-08-01 16:59:10 +0630

    feat: add TVShowsPage catalogue view for /tv and route /tv/:id to ClientDetailPage

--- app/tv/page.js
+++ app/tv/page.js
@@ -1,7 +1,21 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import ClientDetailPage from '@/components/ClientDetailPage';
+import TVShowsPage from '@/components/TVShowsPage';
 
-export const dynamic = 'force-static';
+export default function TVPage() {
+  const pathname = usePathname();
+  const pathParts = pathname ? pathname.split('/').filter(Boolean) : [];
+  
+  // If the URL is /tv/:id (e.g. /tv/2316 or /tv/1339713), render the show Detail View
+  const possibleId = pathParts.length > 1 ? pathParts[1] : null;
+  const isDetailPage = possibleId && possibleId !== 'tv';
 
-export default function TVDetailsPage() {
-  return <ClientDetailPage mediaType="tv" />;
+  if (isDetailPage) {
+    return <ClientDetailPage mediaType="tv" />;
+  }
+
+  // Otherwise (URL is /tv), render the TV Shows Catalogue Page
+  return <TVShowsPage />;
 }
